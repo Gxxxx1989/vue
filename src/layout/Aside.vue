d380c99c5e3b193ba68d73db11556afbd3fc315b
guoxi <guoxi@tg-hd.com>
2022-05-06 10:48:53 +0800

    菜单配置

--- src/layout/Aside.vue
+++ src/layout/Aside.vue
@@ -1,17 +1,14 @@
 <template>
-  <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose"
-           :collapse="isCollapse" text-color="#333" active-text-color="#ffd04b" router="true">
+  <el-menu  class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose"
+           :collapse="isCollapse" text-color="#333" active-text-color="#ffd04b" :router="true" mode="vertical">
     <el-submenu index="1">
       <template slot="title">
         <i class="el-icon-location"></i>
         <span slot="title">导航一</span>
       </template>
       <el-menu-item-group>
-        <span slot="title">分组一</span>
         <el-menu-item index="1-1">选项1</el-menu-item>
         <el-menu-item index="1-2">选项2</el-menu-item>
-      </el-menu-item-group>
-      <el-menu-item-group title="分组2">
         <el-menu-item index="1-3">选项3</el-menu-item>
       </el-menu-item-group>
       <el-submenu index="1-4">
